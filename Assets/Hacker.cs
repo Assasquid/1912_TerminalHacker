8a06b95726b126c38137a9d7690d8f6a7bd11432
AssaSquid <jerome@nebula-tales.com>
2019-12-10 07:45:13 +0000

    Different conditions added to the code (including easter eggs).

--- Assets/Hacker.cs
+++ Assets/Hacker.cs
@@ -27,12 +27,62 @@ public class Hacker : MonoBehaviour
         Terminal.WriteLine("Press 1 for YouTube");
         Terminal.WriteLine("Press 2 for the Video Game Archive");
         Terminal.WriteLine("Press 3 for Nebula Tales");
+        Terminal.WriteLine("Type 'menu' to go back.");
         Terminal.WriteLine("Enter your selection :");
     }
 
     void OnUserInput(string input)
     {
         print("Player '" + input + "'");
+        if (input == "1")
+        {
+            Terminal.WriteLine("You chose YouTube.");
+        }
+
+        else if (input == "2")
+        {
+            Terminal.WriteLine("You chose Video Game Archive.");
+        }
+
+        else if (input == "3")
+        {
+            Terminal.WriteLine("You chose Nebula Tales.");
+        }
+
+        else if (input == "menu")
+        {
+            ShowMainMenu();
+        }
+
+        else if (input == "woomy")
+        {
+            Terminal.WriteLine("Agent 3, is that you ?");
+        }
+
+        else if (input == "evilsquid")
+        {
+            Terminal.WriteLine("Qui pour un Poulpe Garou?");
+        }
+
+        else if (input == "leo paul")
+        {
+            Terminal.WriteLine("Previously in the LPCU...");
+        }
+
+        else if (input == "octoboy")
+        {
+            Terminal.WriteLine("Leo est un Poulpe Garou.");
+        }
+
+        else if (input == "banana croissant")
+        {
+            Terminal.WriteLine("Mmmmm YUMMY !");
+        }
+
+        else
+        {
+            Terminal.WriteLine("Please type a valid command.");
+        }
     }
 
 }
